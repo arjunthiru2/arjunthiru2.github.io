5afbd6f641d48149be508ee056f83e3fd7f78b27
arjunthiru2 <athiru.rajah@gmail.com>
2023-11-30 15:06:51 -0500

    launch update 1

--- script.js
+++ script.js
@@ -11,3 +11,13 @@ function opentab(tabName) {
     document.getElementById(tabName).style.display = "block";
     event.currentTarget.classList.add("active-link");
 }
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzdqi2zcrV1pz6tBn1qMnSjmxpg8L9K2Ot4mhuhmDu5DQJAzkA4MwCSVRkytOTe8mQN/exec';
+  const form = document.forms['submit-to-google-sheet'];
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => console.log('Success!', response))
+      .catch(error => console.error('Error', error.message))
+  })
